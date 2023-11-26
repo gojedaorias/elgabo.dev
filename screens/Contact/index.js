0d358ad309f7bbef73f60c16c936cb6082565e25
Ángel Uriostegui <angelCL0@outlook.com>
2023-11-26 13:49:22 -0500

    fixed

--- screens/Contact/index.js
+++ screens/Contact/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Hero from "./Hero";
 import Layout from "@/components/Layout";
-import CTA from "@/components/CTA";
 import ContactForm from "./ContactForm";
 import FAQ from "@/components/FAQ";
 
@@ -11,7 +10,6 @@ export default function ContactPage() {
       <Hero />
       <ContactForm />
       <FAQ title="contact-page" />
-      <CTA />
     </Layout>
   );
 }
