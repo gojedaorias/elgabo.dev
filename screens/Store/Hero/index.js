92a3f879fa4a3e91e9207c91df8e5776d0392329
Ángel Uriostegui <angelCL0@outlook.com>
2023-11-25 19:41:20 -0500

    cleaner

--- screens/Store/Hero/index.js
+++ screens/Store/Hero/index.js
@@ -4,9 +4,10 @@ import React from "react";
 import styles from "./hero.module.css";
 import cn from "classnames";
 import { motion } from "framer-motion";
-import { Product, Project } from "@/components/Cards";
+import { Product } from "@/components/Cards";
 import { Store } from "@/components/Icons";
 import { products } from "@/mocks/products";
+import CircularAnimation from "@/components/CircularAnimation";
 
 export default function Hero() {
   return (
@@ -61,45 +62,7 @@ export default function Hero() {
               </motion.button>
             </div>
 
-            <div className={styles.circular_container}>
-              <svg className={styles.rotated_text} viewBox="0 0 200 200">
-                <defs>
-                  <path
-                    id="circle"
-                    d={`
-                    M 100, 100
-                    m -75, 0
-                    a 75,75 0 1,0 150,0
-                    a 75,75 0 1,0 -150,0
-                  `}
-                  />
-                </defs>
-                <text width="400" textLength={Math.PI * 150}>
-                  <textPath
-                    alignmentBaseline="top"
-                    xlinkHref="#circle"
-                    className={cn("caption", styles.text)}
-                    textLength={Math.PI * 150}
-                  >
-                    Explore more ・ Explore more ・ Explore more ・
-                  </textPath>
-                </text>
-              </svg>
-
-              <svg
-                className={styles.vector}
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M2.66006 23.9791L0.102417 21.4214L17.6406 3.88327H1.92931V0.229492H23.852V22.1522H20.1982V6.44091L2.66006 23.9791Z"
-                  fill="#080708"
-                />
-              </svg>
-            </div>
+            <CircularAnimation />
           </div>
         </div>
 
