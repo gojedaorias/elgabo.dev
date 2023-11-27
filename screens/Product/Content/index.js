6feb9ab143aae0afa7a1f95e515eb11dec5fb1b7
Ángel Uriostegui <angelCL0@outlook.com>
2023-11-26 21:28:33 -0500

    fixed

--- screens/Product/Content/index.js
+++ screens/Product/Content/index.js
@@ -2,12 +2,19 @@ import React from "react";
 import styles from "./content.module.css";
 import cn from "classnames";
 import { Overview } from "@/components/Cards";
+import Link from "next/link";
 
 export default function Content({ product }) {
   return (
     <section className={cn("section")}>
       <div className={styles.container}>
         <div className={styles.title_container}>
+          <Link href="/store">
+            <button className={cn("button-stroke", styles.button)}>
+              Go back
+            </button>
+          </Link>
+
           <div className={cn("h2", styles.title)}>{product.title}</div>
           <div className={cn("body-2", styles.description)}>
             {product.description}
